e21c682bb9d66e9f049ce05c9fb28ee9d0f84aa5
Moddwyn <nathaliaandaya@gmail.com>
2022-12-15 15:33:10 -0800

    Questions changes

---
+++ AMACE/Assets/Scripts/QuestionsList.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionsList : MonoBehaviour
+{
+    [SerializeField] List<questions> questionList = new List<questions>();
+
+    public static QuestionsList Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public questions GetRandomQuestion()
+    {
+        if(questionList.Count > 0)
+        {
+            int rand = Random.Range(0, questionList.Count);
+            questions myQuestion = questionList[rand];
+            questionList.RemoveAt(rand);
+
+            return myQuestion;
+        }
+
+        Debug.LogWarning("No more questions to retrieve.");
+        return new questions();
+        
+    }
+}
